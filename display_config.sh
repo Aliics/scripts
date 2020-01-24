2e532fde476a8439e30aa9f161bb2d4a41503df9
Alexander Johnston <Aliics@hotmail.com>
2020-01-24 23:57:07 +1300

    Simplify display_config.sh
    Pointless iterations are being done in the script, should have been
    simplified like this from the beginning.

--- display_config.sh
+++ display_config.sh
@@ -1,45 +1,22 @@
 #! /usr/bin/env bash
 
-# Obtain all connected displays via xrandr
-displays=( $(xrandr -q | grep ' connected ' | cut -d" " -f1) )
+# Collect all connected displays.
+displays=$(xrandr -q | grep ' connected ' | cut -d" " -f1)
 
-# If there is only one display make it 1920x1080, as it is likely my
-# laptop display.
-if [ ${#displays[@]} -lt 2 ]
-then
-    xrandr --output ${displays[0]} --mode 1920x1080
-    exit 0
-fi
+# The beginning of the xrandr command, this should be a constant no matter
+# what is connected.
+xrandr_cli="xrandr --output eDP-1-1 --mode 1920x1080"
 
-# There are multiple displays and the laptop display should be configured to
-# either sit on the right or the left depending on what type it is.
-xrandr_command="xrandr"
-for d in ${displays[@]}
-do
-    mode="1920x1080"
-    dpi="96"
-    position=""
-    primary=""
+# Determine where the laptop display will be positioned relatively to the
+# other display.
+# HDMI-0 = home
+# DP-1-1 = work
+case ${displays[@]}
+in
+    *"HDMI-0"*)
+        xrandr_cli="$xrandr_cli --left-of HDMI-0 --output HDMI-0 --auto";;
+    *"DP-1-1"*)
+        xrandr_cli="$xrandr_cli --right-of DP-1-1 --output DP-1-1 --auto";;
+esac
 
-    # DP-1-1 is the Apple monitor at work, I place my laptop to the right of
-    # it, it has a resolution of 2560x1440.
-    # HDMI-1-1 is my home monitor, which the laptop is placed to the left of 
-    # it, this one uses 1920x1080.
-    case $d
-    in
-    "DP-1-1")
-        mode="2560x1440"
-        dpi="102"
-        position="--right-of $d"
-        primary="--primary ";;
-    "HDMI-1-1")
-        position="--left-of $d"
-        primary="--primary ";;
-    esac
-
-    # Concat this all together to build the xrandr command.
-    xrandr_command="$xrandr_command$position --output $d $primary--mode $mode
-    --dpi $dpi "
-done
-
-$($xrandr_command)
+$($xrandr_cli)
