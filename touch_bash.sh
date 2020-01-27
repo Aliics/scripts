380da8f95b6ede080b1b0c27b3caf7e237597e73
Alexander Johnston <Aliics@hotmail.com>
2020-01-27 14:01:25 +1300

    Add overwrite prompt

--- touch_bash.sh
+++ touch_bash.sh
@@ -9,11 +9,25 @@ if [[ "$name" != *".sh" ]]
 then
     read -p "Would you like to append '.sh'? [y/N] " reply
     
-    # I don't actually care about anything other than yes.
+    # I don't actually care about anything other than yes, and the syntax
+    # is just slighty clearer than an if for this comparison.
     case $reply in
         [yY]) name="$name.sh"
     esac
 fi
+
+# Check to see if an executable file of the same name exists, then prompt
+# whether to overwrite or not.
+if [[ -x $name ]]
+then
+    read -p "Executable named $name already exists. Continue? [y/N] " reply
+
+    # Basically kill the program if user doesn't want to overwrite.
+    case $reply in
+        [nN]) exit 1
+    esac
+fi
+
 
 echo "Creating executable script $name"
 
